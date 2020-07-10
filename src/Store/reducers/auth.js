17ea74d9ff239406bd3094a198924afcc93b4d47
chitesh <chiteshbansal111@gmail.com>
2020-07-10 11:04:45 +0530

    adding auth reducer and actions

--- src/Store/reducers/auth.js
+++ src/Store/reducers/auth.js
@@ -1,10 +1,34 @@
+import * as actionTypes from '../actions/actionTypes';
 const initState = {
     user:{},
     error:null,
     isLoggedIn:false,
+    inProgress:false,
 }
 
 
 export default const reducer = (state,action) =>{
+    switch(action.type){
+        case actionTypes.LOGIN_START:
+            return{
+                ...state,
+                inProgress:true,
+            };
+        case actionTypes.LOGIN_SUCCESS:
+            return{
+                ... state,
+                user:action.user,
+                error:null,
+                isLoggedIn:true,
+                inProgress:false,
+            };
+        case actionTypes.LOGIN_FAIL:
+            return{
+                error:action.error,
+                inProgress:false,
+            };
+        default:
+            return state;
+    }
     
 }
